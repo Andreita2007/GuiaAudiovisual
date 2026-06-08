b8fe8eef16bfc3fadd050abbaf0a4fb6600bc9df
Andreita2007 <andreita.github@gmail.com>
2026-06-08 18:48:02 -0400

    Versión sin imagen

--- guia/models.py
+++ guia/models.py
@@ -12,7 +12,6 @@ class ContenidoAudiovisual(models.Model):
     descripcion = models.TextField(verbose_name="Párrafo Informativo")
     video_url = models.URLField(max_length=500, blank=True, null=True, verbose_name="Enlace de Youtube")
     anio = models.CharField(max_length=10, choices=OPCIONES_ANIO, verbose_name="Año Escolar")
-    imagen = models.ImageField(upload_to='guia_fotos/', blank=True, null=True, verbose_name = "Imagen Explicativa")
     
     def __str__(self):
         return f"{self.titulo} - {self.get_anio_display()}"
